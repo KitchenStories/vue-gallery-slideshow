69c3bc74858e8979211f01b9d0a51e34cd3c8ea5
Mihail0v <Mihail0v@mail.com>
2019-03-07 02:37:51 +0700

    Fixed rollup build trouble (#15)

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 import vue from 'rollup-plugin-vue';
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   input: 'src/index.js',
   plugins: [
+    commonjs(),
     vue(),
     babel({
       babelrc: true,
